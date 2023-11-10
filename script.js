ff66ef25b4a8c193474747a44f6d3e9f99874b1a
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-09 17:52:31 -0800

    compleated the first task, creating a greetings function

---
+++ script.js
@@ -0,0 +1,27 @@
+// Write a function that outputs a greeting. It should take an argument “name” and greet the individual.
+// Call your function using console.log(myFunction(“some name”));
+
+
+function greet(name) {
+    console.log("Greetings," + " " + name);
+}
+greet("Lacey");
+
+
+
+// Write a function that gives the approximate circumference of a circle given the diameter.
+
+// Write one function that calls another function.
+
+// Inside of the first function, insert a console.log(...) statement.
+
+// Next, insert the call to the second function.
+
+// The second function should have its own console.log(...) statement inside.
+
+// Finally, after the call to the second function, insert another console.log(...) statement.
+
+// Call your first function in your program and observe the output. What happens in what order and why?
+
+// Write a function that causes the stack to overflow.
+
