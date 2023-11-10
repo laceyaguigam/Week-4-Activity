cd326f6d8d78f3a4f8d7c07f27ca645a228bfb59
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-09 19:08:25 -0800

    completed task 3 and 4, creating a callback function and creating a function that causes the stack to overflow

--- script.js
+++ script.js
@@ -19,16 +19,32 @@ console.log(findCircumference(10)); //make sure to use console.log or it doesn't
 
 
 // Write one function that calls another function.
-
 // Inside of the first function, insert a console.log(...) statement.
-
 // Next, insert the call to the second function.
-
 // The second function should have its own console.log(...) statement inside.
-
 // Finally, after the call to the second function, insert another console.log(...) statement.
+
+
+function call1(firstCall) {
+    console.log("Which call are you?");
+    firstCall();
+}
+function call2() {
+    console.log("Second call!");
+}
+call2(call1);
+
 
 // Call your first function in your program and observe the output. What happens in what order and why?
 
+//when i called the first function it output "Second call!"
+
 // Write a function that causes the stack to overflow.
 
+function dog() {
+    return puppy();
+};
+function puppy() {
+    return dog();
+}
+console.log(dog());
